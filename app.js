0b2db60429d0196263ff058972aca4f685bd46b2
Glitch (panoramic-alive-payment) <none>
2021-03-21 12:16:45 +0000

    ⚽️🌵 Checkpoint ./konular.html:19055815/410 ./style.css:19055815/629 ./index.html:19055815/13

--- app.js
+++
@@ -1,7 +0,0 @@
-const request = require('request');
-const url = require("url");
-var express = require('express');
-var app = express();
-const moment = require("moment");
-require("moment-duration-format");
-const path = require("path");
