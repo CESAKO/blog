d3d67cdfe6c58cac0386611668e12bfd76473fc1
Glitch (panoramic-alive-payment) <none>
2021-03-21 11:53:53 +0000

    🐣😜 Checkpoint ./index.html:19055815/34 ./package.json:19055815/478 ./app.js:19055815/358

---
+++ app.js
@@ -0,0 +1,10 @@
+const request = require('request');
+const url = require("url");
+var express = require('express');
+var app = express();
+const moment = require("moment");
+require("moment-duration-format");
+const path = require("path");
+app.get("/bots", (req, res) => {
+(res, req, ".html")
+});
